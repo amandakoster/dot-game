af189d1a291448881c8a7f34d7632a6b7b1822a5
Amanda Koster <amandakoster@gmail.com>
2017-08-24 11:29:01 -0700

    safety commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from {react}
+import ReactDom from 'react-dom'
+
+
+render() {
+  return
+  <main className='app'>
+  <h1> Dot game </h1>
+  </main>
+}
+
+ReactDom.render(App/>. document.getElementById('root'));
