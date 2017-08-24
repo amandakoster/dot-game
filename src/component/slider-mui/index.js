ece183ad66fa2f9540dae2960d909563990f316b
Amanda Koster <amandakoster@gmail.com>
2017-08-24 16:32:00 -0700

    slider renders

--- src/component/slider-mui/index.js
+++ src/component/slider-mui/index.js
@@ -1,16 +1,24 @@
+import './_slider.scss'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Slider from 'material-ui/Slider';
+
+// handleChange = (slider, value) => {
+//   const newState = {};
+//   newState[slider] = value;
+//   this.setState(newState);
+// };
 
 class SliderMui extends React.Component{
 render(){
   return(
 
-      <div className='slider'>
+      <div >
         <MuiThemeProvider>
-        <Slider />
-        <Slider defaultValue={0.5} />
-        <Slider defaultValue={1} />
+        <Slider
+
+        defaultValue={0.5}
+        />
       </MuiThemeProvider>
       </div>
 
