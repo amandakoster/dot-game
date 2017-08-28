3a15ea2f6e81983802a8f3c47906dc740d71ad98
Amanda Koster <amandakoster@gmail.com>
2017-08-28 11:04:21 -0700

    added css-loader

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
 import App from './component/app';
-import './style/style.scss';
+
 
 class AppContainer extends React.Component {
 
